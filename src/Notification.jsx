7f1defbe1cbd6b3b227aa87370dc1bcb3164a9dc
annienf17 <a.ulanskaxxi@gmail.com>
2024-08-22 12:50:44 +0100

    Notification update with propTypes

--- src/Notification.jsx
+++ src/Notification.jsx
@@ -1,5 +1,10 @@
+import PropTypes from "prop-types";
 import React from "react";
 
 export default function Notification({ message }) {
   return <p>{message}</p>;
 }
+
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
+};
